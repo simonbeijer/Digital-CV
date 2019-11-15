d5af295bdbe91c962a310fabd4ac0d17ffb20721
Simon Beijer <beijersimon@gmail.com>
2019-11-15 14:03:38 +0100

    Fixed sidbar and downmenu

--- js/main.js
+++ js/main.js
@@ -7,91 +7,195 @@ window.addEventListener('load', onLoadFunctions)
  * 
  */
 function onLoadFunctions() {
-    // createConsts();
+    resizedFunction();
+    scrolled();
+    createVariables();
+}
+
+window.addEventListener('resize', resizedFunction);
+window.addEventListener("scroll", scrolled);
+document.querySelector('.media-button').addEventListener('click', displayMenuFunction);
+
+/* width and height variables*/
+/**
+ * 
+ */
+function createVariables() {
+    let windowWidth = document.querySelector('.col-pic-div').offsetWidth;
+    let scrollTest = document.documentElement.scrollTop;
+    return scrollTest ,windowWidth;
 }
 
 
-// function createConsts() {
-//     const displayMenu = document.querySelector('#display-menu');
-//     console.log('displayMenu:', displayMenu)
-// }
+
+/* SCROLLFUNCTION */
+/**
+ * 
+ */
+function scrolled() {
+    console.log('SCROLLED');
+
+    let windowWidth = document.querySelector('.col-pic-div').offsetWidth;
+    if (windowWidth <= 798) {
+        downMenu();
+        barFunction();
+    } if (windowWidth > 798) {
+        sidebarFunction();
+    }
+}
+
+
+/* RESIZEDFUNCTION */
+
+
+/**
+ * 
+ */
+function resizedFunction() {
+    console.log('RESIZED');
+    let windowWidth = document.querySelector('.col-pic-div').offsetWidth;
+    if (windowWidth <= 798) {
+        downMenu();
+        barFunction()
+    } if (windowWidth > 798) {
+        document.querySelector('.down-menu').style.display = "none";
+        calculateSidebar();
+    }
+}
 
 
 
-// document.querySelector('#display-menu').addEventListener('click', displayMenuFunction);
+
 let clicked = false;
-function displayMenuFunction() {
-    
-    console.log('clicked:', clicked)
 
-    console.log("display");
-    if(clicked === false) {
-        document.querySelector('.sidebar').style.display = "block";
-        document.querySelector('.sidebar').style.position = "absolute";
-        document.querySelector('.sidebar').style.width = "100%";
+/* DISPLAY MENU */
+
+/**
+ * 
+ */
+function displayMenuFunction() {
+
+    console.log("display-menu");
+    if (clicked === false) {
+        document.querySelector('.down-menu').style.position = "fixed";
+        document.querySelector('.down-menu').style.display = "flex";
         clicked = true;
         console.log('clicked:', clicked)
     } else if (clicked === true) {
-        document.querySelector('.sidebar').style.display = "none";
+        document.querySelector('.down-menu').style.display = "none";
         clicked = false;
         console.log('clicked:', clicked)
     }
-    
+
 }
 
+/* DOWNMENU */
 
-// $(document).ready(function () {
-//     $(this).scrollTop(0);
-// });
+/**
+ * 
+ */
+function downMenu() {
+    let scroll2 = $(this).scrollTop();
+    console.log("test2");
+    let colDivPic2 = document.querySelector('.col-pic-div').offsetHeight;
+    let mediaButton = document.querySelector('.media-button').offsetHeight;
+    let calculate11 = colDivPic2 - scroll2;
+    console.log('calculate11:', calculate11)
 
 
-// window.addEventListener("scroll",test, test2);
-
-
-let width = document.querySelector('.col-pic-div').offsetWidth;
-console.log('width:', width)
-
-if (width < 798) {
-    window.addEventListener("scroll", test2);
-} else {
-    window.addEventListener("scroll", test);
+    console.log('scroll2:', scroll2)
+    console.log('colDivPic2:', colDivPic2);
+    if (calculate11) {
+        document.querySelector(".down-menu").style.marginTop = calculate11 + 60 + "px";
+        document.querySelector(".down-menu div").style.width = "100%";
+    }
+    if (calculate11 <= 0) {
+        document.querySelector(".down-menu").style.marginTop = Math.abs(calculate11) + 60 + "px";
+        document.querySelector(".down-menu div").style.width = "100%";
+    }
+    if (scroll2 >= colDivPic2) {
+        document.querySelector(".down-menu").style.marginTop = "60px";
+        document.querySelector(".down-menu div").style.width = "100%";
+    }
+    if (scroll2 > colDivPic2) {
+        document.querySelector(".media-button").style.position = "sticky";
+        document.querySelector(".media-button").style.top = "0";
+    }
 }
 
-function test() {
+/* DOWNMENU CLICKED */
+
+
+/**
+ * 
+ */
+document.querySelector('.down-menu div').addEventListener("click", function () {
+    document.querySelector('.down-menu').style.display = "none";
+    clicked = false;
+    console.log("CLICKED");
+});
+
+
+/* SIDEBAR */
+
+/**
+ * 
+ */
+function calculateSidebar() {
     let colDivPic = document.querySelector('.col-pic-div').offsetHeight;
-    console.log('colDivPic:', colDivPic);
     let col1Div = document.querySelector('.grid-col-1').offsetHeight;
-    console.log('col1Div:', col1Div);
     let col2Div = document.querySelector('.grid-col-2').offsetHeight;
-    console.log('col2Div:', col2Div);
     let col3Div = document.querySelector('.grid-col-3').offsetHeight;
-    console.log('col3Div:', col3Div);
     let col4Div = document.querySelector('.grid-col-4').offsetHeight;
-    console.log('col4Div:', col4Div);
     let col5Div = document.querySelector('.grid-col-5').offsetHeight;
-    console.log('col5Div:', col5Div);
 
-    console.log('col1Div:', col1Div);
+    let scroll = $(this).scrollTop();
+    let scrollTest = document.body.scrollTop;
+    console.log('scrollTest:', scrollTest);
+    console.log('scroll:', scroll);
+
+    let calculate = colDivPic - scroll;
+
+    let calculate5 = colDivPic * 0.7;
+
+    if (calculate) {
+        document.querySelector(".sidebar").style.marginTop = calculate + "px";
+    } if (scroll > calculate5) {
+        document.querySelector(".sidebar").style.position = "fixed";
+        document.querySelector(".sidebar").style.margin = "30vh 0 30vh 85vw";
+    }
+}
+
+
+/**
+ * 
+ */
+function sidebarFunction() {
+    let colDivPic = document.querySelector('.col-pic-div').offsetHeight;
+    let col1Div = document.querySelector('.grid-col-1').offsetHeight;
+    let col2Div = document.querySelector('.grid-col-2').offsetHeight;
+    let col3Div = document.querySelector('.grid-col-3').offsetHeight;
+    let col4Div = document.querySelector('.grid-col-4').offsetHeight;
+    let col5Div = document.querySelector('.grid-col-5').offsetHeight;
+
     let scroll = $(this).scrollTop();
     console.log('scroll:', scroll);
-    wH = $(window).height();
+    let scrollTest = document.documentElement.scrollTop;
+    console.log('scrollTest:', scrollTest);
 
-    // const heightOfSCroll = documnet.querySelector('scroll');
     let calculate = colDivPic - scroll;
 
     let calculate5 = colDivPic * 0.7;
     let cutDiv = col1Div * 0.1;
 
+    let cutDiv2 = col1Div * 0.3;
+
     if (calculate) {
         document.querySelector(".sidebar").style.marginTop = calculate + "px";
-        document.querySelector(".sidebar").style.display = "flex";
     } if (scroll > calculate5) {
         document.querySelector(".sidebar").style.position = "fixed";
         document.querySelector(".sidebar").style.margin = "30vh 0 30vh 85vw";
     }
-
-    cutDiv2 = col1Div * 0.3;
-
     if (scroll > 1) {
         document.querySelector(".nav-1 a").style.background = "black";
     } if (scroll > col1Div - cutDiv) {
@@ -118,91 +222,31 @@ function test() {
     } if (scroll < col1Div + col2Div + col3Div + col4Div - cutDiv2) {
         document.querySelector(".nav-5 a").style.background = "grey";
     }
-}
 
-function test2() {
-    console.log("test2");
-    let colDivPic2 = document.querySelector('.col-pic-div').offsetHeight;
-    let calculate10 = 60 + colDivPic2;
-    console.log('calculate10:', calculate10)
-    console.log('colDivPic2:', colDivPic2);
-    if (calculate10) {
-        document.querySelector(".sidebar").style.marginTop = calculate10 + "px";
-        document.querySelector(".sidebar").style.width = "100%";
-
+    if (scroll > col1Div && clicked === false) {
+        document.querySelector(".media-button").style.position = "sticky";
+        document.querySelector(".media-button").style.top = "0";
+        console.log("funkar");
+    }
+    if (scroll > col1Div && clicked === true) {
+        document.querySelector(".media-button").style.position = "sticky";
+        document.querySelector(".media-button").style.top = "0";
+        console.log("funkar");
     }
 }
 
 
-
-
-
-// $(window).scroll(function() {
-//     let colPicT = $('.col-pic-div').offset().top,
-//         colPicH = $('.col-pic-div').outerHeight(),
-//         col1T = $('.col-1-div').offset().top,
-//         col1H = $('.col-1-div').outerHeight(),
-//         wH = $(window).height(),
-//         wS = $(this).scrollTop();
-//      console.log((colPicT-colPicH) , wS);
-//     if (wS > (colPicT+colPicH-wH)){
-//         document.querySelector(".nav-1 a").style.background = "black";
-//     } else if (wS > (col1T+col1H-wH) && wS < (colPicT+colPicH-wH) ) {
-//         document.querySelector(".nav-1 a").style.background = "rgba(133, 130, 130, 0.808)";
-//         document.querySelector(".nav-2 a").style.background = "black";
-//     }
-// });
-
-// $(window).scroll(function() {
-//     var hT = $('.grid-col-1').offset().top,
-//         hH = $('.grid-col-1').outerHeight(),
-//         wH = $(window).height(),
-//         wS = $(this).scrollTop();
-//      console.log((hT-wH) , wS);
-//     if (wS > (hT+hH-wH)){
-//         document.querySelector(".nav-1 a").style.background = "black";
-//     }
-// });
+function barFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    console.log('winScroll:', winScroll)
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    console.log('height:', height)
+    var scrolled = (winScroll / height) * 100;
+    console.log('scrolled:', scrolled)
+    document.getElementById("bar").style.width = scrolled + "%";
+}
 
 
 // instead of href?? //
 // var elmnt = document.getElementById("content");
 // elmnt.scrollIntoView();
-
-
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function () { myFunction() };
-
-// function myFunction() {
-//     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-//     console.log('winScroll:', winScroll)
-//     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//     console.log('height:', height)
-//     var scrolled = (winScroll / height) * 100;
-//     console.log('scrolled:', scrolled)
-//     document.getElementById("myBar").style.width = scrolled + "%";
-//     if (winScroll === 0) {
-//         document.querySelector("aside").style.display = "flex";
-//         document.querySelector("aside").style.marginTop = "99vh";
-//     } if (winScroll > 400) {
-//         document.querySelector("aside").style.display = "flex";
-//         document.querySelector("aside").style.margin = "30vh 0 30vh 78vw;";
-//     }
-//     // if (winScroll > 800) {
-//     //     document.querySelector("aside").style.display = "flex";
-//     //     document.querySelector(".aside-1").style.background = "blue";
-//     // } if (winScroll > 1200) {
-//     //     document.querySelector("aside").style.display = "flex";
-//     //     document.querySelector(".aside-1").style.background = "white";
-//     //     document.querySelector(".aside-2").style.background = "blue";
-//     // } if (winScroll > 1600) {
-//     //     document.querySelector("aside").style.display = "flex";
-//     //     document.querySelector(".aside-2").style.background = "white";
-//     //     document.querySelector(".aside-3").style.background = "blue";
-//     // }
-//     //  else if (winScroll < 800) {
-//     // document.querySelector("aside").style.display = "none";
-//     // document.querySelectorAll("aside").style.background = "white";
-//     // }
-// }
-
